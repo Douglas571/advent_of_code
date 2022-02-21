277ffac74b1a19734e1aaecd13448cfc04efdef9
Douglas Socorro <douglassocorro.work@gmail.com>
2022-02-21 12:28:28 -0400

    progress

--- 4/bingo.py
+++ 4/bingo.py
@@ -1,30 +1,44 @@
+class Result:
+  self.winner_board
+  self.last_number
+  self.sum_unmarket_numbs
+
 def bingo_game(numbers, boards):
   pass
 
+
+
+def extract_numbers(raw_lines):
+  numbers = raw_lines[0].split(',')
+  numbers = [ int(n) for n in numbers ]  
+
+  return numbers
+
+def extract_boards(raw_lines):
+  boards = []
+  for i in range(2, len(raw_lines), 6):
+    board = []
+    for j in range(i, i+5):
+      row = raw_lines[j].strip().replace('  ', ' ').split(' ')
+
+      row = [ int(n) for n in row ]
+      board.append(row)
+    boards.append(board)
+
+  return boards
+
+def extract_input(raw_lines):
+  numbers = extract_numbers(raw_lines)
+  boards = extract_boards(raw_lines)    
+
+  return numbers, boards
+
 def main():
   with open("example.txt") as f:
-    data = f.readlines()
+    raw_lines = f.readlines()
+    numbers, boards = extract_input(raw_lines)
 
-    numbers = data[0].split(',')
-    numbers = [ int(n) for n in numbers ]
-    print(numbers)
-
-    print('--- boards ---')
-    boards = []
-    board = []
-    for i, row in enumerate(data):
-      if i == 0 or len(row.strip()) < 1: 
-        continue
-
-      if len(board) < 5:
-        board.append(row)
-
-      boards.append(row)
-      
-      board = []
-      board.append(row)
-
-    print(boards)
+    winner = bingo_game(numbers, boards)
 
 if __name__ == '__main__':
   main()
--- 4/test.py
+++ 4/test.py
@@ -4,14 +4,40 @@ from bingo import *
 class TestStringMethods(unittest.TestCase):
   def test_one(self):
     with open('example.txt') as f:
-      numbers, boards = extrar_input(f.readline())
+      raw_lines = f.readlines()
+      numbers, boards = extract_input(raw_lines)
 
       exp_numbers = [7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1]
-      exp_boards = []
+      exp_boards = [
+        [
+          [22, 13, 17, 11, 0],
+          [8, 2, 23, 4, 24],
+          [21, 9, 14, 16, 7],
+          [6, 10, 3, 18, 5],
+          [1, 12, 20, 15, 19],
+        ],
+        [
+          [3, 15, 0, 2, 22],
+          [9, 18, 13, 17, 5],
+          [19, 8, 7, 25, 23],
+          [20, 11, 10, 24, 4],
+          [14, 21, 16, 12, 6],
+        ],
+        [
+          [14, 21, 17, 24, 4],
+          [10, 16, 15, 9, 19],
+          [18, 8, 23, 26, 20],
+          [22, 11, 13, 6, 5],
+          [2, 0, 12, 3, 7],
+        ]
+      ]
       self.assertEqual(exp_numbers, numbers)
       self.assertEqual(exp_boards, boards)
 
-      winner = bingo_game(numbers, boards)
+      results = bingo_game(numbers, boards)
+      self.assertEqual(3, results.winner_board)
+      self.assertEqual(24, results.last_number)
+      self.assertEqual(180, results.sum_unmarket_numbs)
       
 if __name__ == '__main__':
   unittest.main()
