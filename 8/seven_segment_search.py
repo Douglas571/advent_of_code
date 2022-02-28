f1ee9f1765ccd7931a3f4b385f2911dc6653ea15
Douglas Socorro <douglassocorro.work@gmail.com>
2022-02-28 08:52:26 -0400

    Finished the 8th day puzzle

--- 8/seven_segment_search.py
+++ 8/seven_segment_search.py
@@ -1,10 +1,25 @@
-from collections import Counter
-
-def extract_four_ouput_digits(raw_lines):
-  fods = []
+def extrat_digits(raw_lines):
+  digits = []
 
   for line in raw_lines:
-    fods.append(line.strip('\n').split(' | ')[1].split(' '))
+    entry = { "output": [], "input": [] }
+
+    raw_input_digits = line.strip('\n').split(' | ')[0].split(' ')
+    for d in raw_input_digits:
+      entry["input"].append(str().join(sorted(d)))
+    
+    raw_output_digits = line.strip('\n').split(' | ')[1].split(' ')
+    for d in raw_output_digits:
+      entry["output"].append(str().join(sorted(d)))
+
+    digits.append(entry)
+
+  return digits
+
+def get_four_output_digits(digits):
+  fods = []
+  for row in digits:
+    fods.append(row['output'])
 
   return fods
 
@@ -18,13 +33,101 @@ def count_unique_pattern_numbers(rows_of_digits):
 
   return count 
 
+def get_unique_pattern_digits(entry):
+  decoded_digits = {}
+
+  for d in entry['input']:
+    number_of_segments = len(d)
+    match(number_of_segments):
+      case 2:
+        decoded_digits[d] = '1'
+        one = d
+        
+      case 3:
+        decoded_digits[d] = '7'
+
+      case 4:
+        decoded_digits[d] = '4'
+        four = d
+
+      case 7:
+        decoded_digits[d] = '8'
+
+  return decoded_digits, one, four
+
+def get_complex_digits(entry, one, four):
+  decoded_digits = {}
+  for d in entry['input']:
+    number_of_segments = len(d)
+    one = set(one)
+    four = set(four)
+    unknow = set(d)
+
+    match(number_of_segments):
+      case 5:
+        one_rest = one - unknow
+        four_rest = (four - one) - unknow
+
+        if len(one_rest) == 0:
+          decoded_digits[d] = '3'
+        
+        if len(four_rest) == 0:
+          decoded_digits[d] = '5'
+
+        if len(four_rest) == 1 and len(one_rest) == 1:
+          decoded_digits[d] = '2'
+
+      case 6:
+        one_rest = one - unknow
+        four_rest = (four - one) - unknow
+
+        if len(one_rest) == 1:
+          decoded_digits[d] = '6'
+
+        if len(one_rest) == 0 and len(four_rest) == 1:
+          decoded_digits[d] = '0'
+
+        if len(one_rest) == 0 and len(four_rest) == 0:
+          decoded_digits[d] = '9'
+
+  return decoded_digits
+
+def decode_digits(digits):
+  res = []
+
+  for entry in digits:
+    decoded_digits = {}
+    
+    ( decoded_digits,
+      one, 
+      four ) = get_unique_pattern_digits(entry)
+    complex_decodeds = get_complex_digits(entry, one, four)
+    decoded_digits.update(complex_decodeds)
+
+    entry['decoded_digits'] = decoded_digits
+    entry['output_v'] = ''
+    for d in entry['output']:
+      entry['output_v'] += decoded_digits[d]
+
+    res.append(entry)
+
+  return res
+
 def main():
   with open('input.txt') as f:
-    fods = extract_four_ouput_digits(f.readlines())
+    digits = extrat_digits(f.readlines())
+    fods = get_four_output_digits(digits)
     upn = count_unique_pattern_numbers(fods)
     print('--- Seven Segment Search ---')
     print('1th part:')
     print(f'the numbs of unique patters are: {upn}')
+
+    print('2th part:')
+    res = decode_digits(digits)
+    sumatory = 0
+    for entry in res:
+      sumatory += int(entry['output_v'])
+    print(f'the sum of all entries is: {sumatory}')
 
 if __name__ == '__main__':
   main()
--- 8/test.py
+++ 8/test.py
@@ -4,11 +4,48 @@ from seven_segment_search import *
 class Test_Case(unittest.TestCase):
   def test_one(self):
     with open('example.txt') as f:
-      fods = extract_four_ouput_digits(f.readlines())
-      self.assertEqual(fods[2], ["cg", "cg", "fdcagb", "cbg"])
+      data = extrat_digits(f.readlines())
+      #self.assertEqual(data[2]["output"], ["cg", "cg", "fdcagb", "cbg"])
 
+      fods = get_four_output_digits(data)
       upns = count_unique_pattern_numbers(fods)
       self.assertEqual(upns, 26)
+
+  def test_decoder(self):
+    exp_digits = {
+      "abcdefg": "8",
+      "abd": "7",
+      "abef": "4",
+      "ab": "1",
+      "bcdef": "5",
+      "acdfg": "2",
+      "abcdf": "3",
+      "abcdef": "9",
+      "bcdefg": "6",
+      "abcdeg": "0",
+    }
+
+
+    with open('base-example.txt') as f:
+      digits = extrat_digits(f.readlines())
+      entry = digits[0]
+
+      #self.assertEqual(entry['output'], ["cdfeb", "fcadb", "cdfeb", "cdbaf"])
+      self.assertEqual(entry['input'][-1], "ab")
+
+      res = decode_digits(digits)
+      self.assertEqual(len(res), 1)
+      self.assertEqual(exp_digits, res[0]['decoded_digits'])
+      self.assertEqual(res[0]['output_v'], "5353")
+
+  def test_three(self):
+    with open('example.txt') as f:
+      digits = extrat_digits(f.readlines())
+
+      res = decode_digits(digits)
+      self.assertEqual(res[3]['output_v'], "9361")
+      self.assertEqual(res[-1]['output_v'], "4315")
+
 
 if __name__ == '__main__':
   unittest.main()
