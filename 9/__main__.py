3e8a25540000fc8e7e03f2eaddd6eebd7d043fb6
Douglas Socorro <douglassocorro.work@gmail.com>
2022-03-05 15:09:52 -0400

    there's a bug

--- 9/__main__.py
+++ 9/__main__.py
@@ -3,11 +3,14 @@ from smoke_basin import *
 def main():
   with open('input.txt') as f:
     raw_lines = f.readlines()
-  
-    solution = get_1th_solution(raw_lines)
-
+    
     print('--- Day 9: Smoke Basin ---')
-    print(f'1t part\n the sum of risk levels are: {solution}')
+
+    solution = get_1th_solution(raw_lines)
+    print(f'1t part:\n the sum of risk levels are: {solution}')
+
+    second_solution = get_2th_solution(raw_lines)
+    print(f'2t part:\n the product of largest basins are: {second_solution}')    
 
 if __name__ == '__main__':
   main()
--- 9/smoke_basin.py
+++ 9/smoke_basin.py
@@ -68,7 +68,7 @@ def get_lowest_points(m):
       #print(f'New low: new_i={new_i}; new_j={new_j}')        
 
   return lowest_points
-  # wrong guess 1815: so higth
+  # wrong guess 1815: too higth
 
 def get_risk_levels(points):
   risk_levels = [ p + 1 for p in points]
@@ -82,8 +82,39 @@ def get_1th_solution(raw_lines):
 
   return sum_rlvs
 
-def get_basins(m, i, j):
-  pass
+def get_basins(m, i, j, checked_points=[]):
+  basins = []
+  checked_points = checked_points.copy()
+  center = m[i][j]
+  up    = (i-1, j)
+  down  = (i+1, j)
+  rigth = (i, j+1)
+  left  = (i, j-1)
+
+  # iterar sobre los border
+  #print(f'new iter: {i},{j} = {center}')
+  basins.append(center)
+  checked_points.append((i,j))
+  for r,c in [up,rigth,down,left]:
+    #print(f'point ({r},{c})')
+    if (r >= 0 and r < len(m)) and (c >= 0 and c < len(m[r])):
+      
+      if (r, c) in checked_points: 
+        #print(f'ckecked: {r},{c} = {m[r][c]}')
+        continue
+
+      new_p = m[r][c]
+      diff = abs(center - new_p)
+      if diff == 1 and new_p != 9:
+        #print(f'new_p={new_p}')
+
+        sub_basins, checked_points = get_basins(m,r,c, checked_points)
+        basins.extend(sub_basins)
+      #checked_points.append((r, c))
+    # else:
+        # print(f'invalid: r={r},c={c}')
+
+  return basins, checked_points
 
 def get_2th_solution(raw_lines):
   lowest_points = []
@@ -96,9 +127,26 @@ def get_2th_solution(raw_lines):
         lowest_points.append((i, j, m[i][j]))
 
   for i, j, n in lowest_points:
-    basin = get_basins(m, i, j)
+    print(f'\nNEW low point: i={i}, j={j}, n={n}')
+    basin, _ = get_basins(m, i, j)
+    print(f'FINAL basins of i={i}, j={j}, n={n}, len={len(basin)}\n\t{basin}')
     all_basins.append(len(basin))
 
-  print(all_basins)
+  print(f'all_basins={all_basins}')
+  #print(lowest_points)
 
-  print(lowest_points)
+  solution = 1
+  c = 0
+  LIST = list(reversed(sorted(all_basins)))
+  print(LIST)
+  for b in LIST:
+    if c == 3: break
+
+    print(f'b={b}')
+    solution *= b
+    c += 1
+
+  #print(solution)
+
+  return solution
+  # You guessed 712800, is to low.
